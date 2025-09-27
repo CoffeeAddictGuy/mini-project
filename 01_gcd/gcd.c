d5678fc94ff876076fbe47bba8661505b1775007
CoffeeAddictGuy <richcrwo@gmail.com>
2025-09-27 22:31:12 +0300

    ADD gcd code

--- 01_gcd/gcd.c
+++ 01_gcd/gcd.c
@@ -1,4 +1,18 @@
+#include <stdio.h>
 
 int gcd(int first, int second) {
+    while (second != 0) {
+        int temp = first %= second;
+        first = second;
+        second = temp;
+    }
+    return first;
+}
+
+int main() {
+    int a = 102;
+    int b = 465;
+    int res = gcd(a, b);
+    printf("Result: %d\n", res);    
     return 0;
 }
