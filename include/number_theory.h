9ef5a44844b00c72a6e1a1554f31d81accf83baa
CoffeeAddictGuy <richcrwo@gmail.com>
2025-10-10 19:03:34 +0300

    ADD SoE
    Add solution for Sieve of Eratosthenes

--- include/number_theory.h
+++ include/number_theory.h
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
 int gcd(int first, int second);
 int lcmByGCD(int a, int b);
 int lcm(int a, int b);
+void sieveOfEratosthenes(int* array, int n);
--- src/math/01_number_theory/number_theory.c
+++ src/math/01_number_theory/number_theory.c
@@ -1,38 +1,41 @@
 #include <number_theory.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int gcd(int first, int second) {
-    while (second != 0) {
-        int temp = first %= second;
-        first = second;
-        second = temp;
-    }
-    return first;
+  while (second != 0) {
+    int temp = first %= second;
+    first = second;
+    second = temp;
+  }
+  return first;
 }
 
 int lcmByGCD(int a, int b) {
-    int first = a;
-    int second = b;
-    while (b != 0) {
-        int temp = a % b;
-        a = b;
-        b = temp;
+  int first = a;
+  int second = b;
+  while (b != 0) {
+    int temp = a % b;
+    a = b;
+    b = temp;
+  }
+
+  if (first * second < 0)
+    first = first * second * -1;
+  else
+    first = first * second;
+  return first / a;
+}
+
+// Integer factorization
+
+void sieveOfEratosthenes(int *array, int n) {
+  for (int i = 0; i < n; i++) {
+    if (array[i] != 0) {
+      int k = array[i];
+      for (int j = i + k; j < n; j += k)
+        array[j] = 0;
     }
-
-    if (first * second < 0) 
-        first = first * second * -1;
-    else
-        first = first * second;
-    return first / a;
+  }
 }
 
-int lcm(int a, int b) {
-    return 0;
-}
-
-int main() {
-    int a = 102;
-    int b = 465;
-    int res = gcd(a, b);
-    printf("Result: %d\n", res);    
-    return 0;
-}
