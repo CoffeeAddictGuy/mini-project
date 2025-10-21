9114663c2a5fc8ba38d359ec12cdca7bbeb20fa9
Richard <richcrwo@gmail.com>
2025-10-21 18:03:29 +0300

    [ADD] New unit
    Add new unit - 02_Algebra

---
+++ include/algebra.h
@@ -0,0 +1,7 @@
+
+struct quadEq {
+  double x1;
+  double x2;
+};
+
+struct quadEq* quadraticEquation(int *a, int *b, int *c);
---
+++ src/math/02_algebra/algebra.c
@@ -0,0 +1,27 @@
+#include "algebra.h"
+#include <math.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct quadEq *quadraticEquation(int *a, int *b, int *c) {
+  struct quadEq *res = malloc(sizeof(struct quadEq));
+
+  double a_d = (double)*a;
+  double b_d = (double)*b;
+  double c_d = (double)*c;
+
+  double dis = sqrt(b_d * b_d - 4 * a_d * c_d);
+  printf("Dis - %f\n", (-b_d + dis) / (2 * a_d));
+  if (dis > 0) {
+    res->x1 = (-b_d + dis) / (2 * a_d);
+    res->x2 = (-b_d - dis) / (2 * a_d);
+  } else if (dis == 0) {
+    res->x1 = -(b_d / (2 * a_d));
+    res->x2 = 0;
+  } else {
+    res->x1 = 0;
+    res->x2 = 0;
+  }
+  return res;
+}
