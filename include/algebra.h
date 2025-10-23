b2d10a3797361298be7886aaa6e30537f4246779
Richard <richcrwo@gmail.com>
2025-10-23 20:47:10 +0300

    [ADD] New version of polinom and
    Add struct to control x and y in pol.

--- include/algebra.h
+++ include/algebra.h
@@ -8,10 +8,10 @@ struct polDef {
   int c;
   int px;
   int py;
-  int pz;
 };
 
 struct quadEq* quadraticEquation(int *a, int *b, int *c);
 void sumPol(int* f, int* f_s, int* s, int* s_s, int *res);
 void subPol(int* f, int* f_s, int* s, int* s_s, int *res);
 void mulPol(int* f, int* f_s, int* s, int* s_s, int *res);
+void printPol(struct polDef* pd, int* s);
