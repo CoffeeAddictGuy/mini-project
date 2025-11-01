d1998118161672d63a11489aee6a06bd05d1440c
Richard <richcrwo@gmail.com>
2025-11-01 16:19:51 +0300

    [UPDATE] divPol
    Update divPol by add rem and remsize

--- include/algebra.h
+++ include/algebra.h
@@ -17,6 +17,7 @@ struct quadEq* quadraticEquation(int *a, int *b, int *c);
 void sumPol(struct polDef* pd, int* f_s, struct polDef* s, int* s_s, struct polDef *res, int *rsize);
 void subPol(struct polDef *f, int *f_s, struct polDef *s, int *s_s, struct polDef *res, int *rsize);
 void mulPol(struct polDef *f, int* f_s, struct polDef *s, int* s_s, struct polDef *res, int *rsize);
+void divPol(struct polDef *f, int* f_s, struct polDef *s, int* s_s, struct polDef *res, int *rsize, struct polDef *rem, int *remsize);
 
 void polSimplefire(struct polDef *pd, int* s);
 void moveNullPol(struct polDef *pd, int* s, int z);
